630b986de31c8e6b4c6b5e9addc770ec281e6ef8
paryushjain <paryushjain@gmail.com>
2022-02-19 22:10:51 +0530

    added optimsed solution for two sum

--- com/coding/questions/interviews/leetcode/TwoSum.java
+++ com/coding/questions/interviews/leetcode/TwoSum.java
@@ -1,5 +1,9 @@
 package com.coding.questions.interviews.leetcode;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author paryjain 
  * https://leetcode.com/problems/two-sum/
@@ -25,11 +29,24 @@ public class TwoSum {
 		return a;
 	}
 
+	public static int[] twoSumOptimised(int[] nums, int target){
+		Map<Integer,Integer> map = new HashMap<>();
+		for(int i=0;i<nums.length;i++){
+			if(map.containsKey(target-nums[i])){
+				return new int[]{map.get(target-nums[i]),i};
+			}else {
+				map.put(nums[i], i);
+			}
+		}
+		return null;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int[] a = new int[] { 2, 7, 11, 15 };
-		twoSum(a, 9);
+		int[] a = new int[] { 3,2,8,4 };
+		int [] ans = twoSumOptimised(a, 6);
+		Arrays.stream(ans).forEach(System.out::println);
 
 	}
 
